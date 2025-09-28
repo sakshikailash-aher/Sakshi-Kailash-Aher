28272681e1dde6374db1d110d380f1cd0cf3f7e7
sakshikailash-aher <sakshi.ahercse2024@indoreinstitute.com>
2025-09-28 18:47:40 +0530

    Add files via upload

---
+++ display array.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int i,n=5,a[5]={10,20,30,40,50};
+	
+	printf("The originally elements of array are:\n");
+	for(i=0;i<n;i++)
+	{
+		printf("a[%d]=%d\n",i,a[i]);
+	}
+}
